baff1bcacaa112be2bf4459c918fc4de54f14c7e
Oscar Adimi <tuscar2001@gmail.com>
2023-11-20 08:47:47 -0800

    updates

--- app.py
+++ app.py
@@ -1,32 +1,112 @@
+import json
 import os
+import time
+from flask import Flask, request, jsonify
+import openai
+from openai import OpenAI
+import functions
 
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+# Check OpenAI version compatibility
+from packaging import version
 
+required_version = version.parse("1.1.1")
+current_version = version.parse(openai.__version__)
+OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
+# OPENAI_API_KEY = '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'
+if current_version < required_version:
+  raise ValueError(
+      f"Error: OpenAI version {openai.__version__} is less than the required version 1.1.1"
+  )
+else:
+  print("OpenAI version is compatible.")
+
+# Create Flask app
 app = Flask(__name__)
 
+# Initialize OpenAI client
+client = OpenAI(api_key=OPENAI_API_KEY)
 
-@app.route('/')
-def index():
-   print('Request for index page received')
-   return render_template('index.html')
+# Create or load assistant
+assistant_id = functions.create_assistant(
+    client)  # this function comes from "functions.py"
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
+# Start conversation thread
+@app.route('/start', methods=['GET'])
+def start_conversation():
+  print("Starting a new conversation...")
+  thread = client.beta.threads.create()
+  print(f"New thread created with ID: {thread.id}")
+  return jsonify({"thread_id": thread.id})
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+
+# Generate response
+@app.route('/chat', methods=['POST'])
+def chat():
+  data = request.json
+  thread_id = data.get('thread_id')
+  user_input = data.get('message', '')
+
+  if not thread_id:
+    print("Error: Missing thread_id")
+    return jsonify({"error": "Missing thread_id"}), 400
+
+  print(f"Received message: {user_input} for thread ID: {thread_id}")
+
+  # Add the user's message to the thread
+  client.beta.threads.messages.create(thread_id=thread_id,
+                                      role="user",
+                                      content=user_input)
+
+  # Run the Assistant
+  run = client.beta.threads.runs.create(thread_id=thread_id,
+                                        assistant_id=assistant_id)
+
+  # Check if the Run requires action (function call)
+  while True:
+    run_status = client.beta.threads.runs.retrieve(thread_id=thread_id,
+                                                   run_id=run.id)
+    # print(f"Run status: {run_status.status}")
+    if run_status.status == 'completed':
+      break
+    elif run_status.status == 'requires_action':
+      # Handle the function call
+      for tool_call in run_status.required_action.submit_tool_outputs.tool_calls:
+        if tool_call.function.name == "solar_panel_calculations":
+          # Process solar panel calculations
+          arguments = json.loads(tool_call.function.arguments)
+          output = functions.solar_panel_calculations(
+              arguments["address"], arguments["monthly_bill"])
+          client.beta.threads.runs.submit_tool_outputs(thread_id=thread_id,
+                                                       run_id=run.id,
+                                                       tool_outputs=[{
+                                                           "tool_call_id":
+                                                           tool_call.id,
+                                                           "output":
+                                                           json.dumps(output)
+                                                       }])
+        elif tool_call.function.name == "create_lead":
+          # Process lead creation
+          arguments = json.loads(tool_call.function.arguments)
+          output = functions.create_lead(arguments["name"], arguments["phone"],
+                                         arguments["address"])
+          client.beta.threads.runs.submit_tool_outputs(thread_id=thread_id,
+                                                       run_id=run.id,
+                                                       tool_outputs=[{
+                                                           "tool_call_id":
+                                                           tool_call.id,
+                                                           "output":
+                                                           json.dumps(output)
+                                                       }])
+      time.sleep(1)  # Wait for a second before checking again
+
+  # Retrieve and return the latest message from the assistant
+  messages = client.beta.threads.messages.list(thread_id=thread_id)
+  response = messages.data[0].content[0].text.value
+
+  print(f"Assistant response: {response}")
+  return jsonify({"response": response})
 
 
 if __name__ == '__main__':
-   app.run()
+  app.run(host='0.0.0.0', port=8080)
